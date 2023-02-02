c6fcbb0b89f302e6520233e5db16e04b0bf487c9
Jakob Platzer <jakob.platzer@gmx.at>
2023-02-02 14:54:09 +0100

    completed challenge

--- advanced-script.js
+++ advanced-script.js
@@ -15,4 +15,18 @@ minutesConverter(8605)
 
 // challenge
 
+// ATM
 
+
+function atm(sum) {
+    if (Number.isInteger(sum/10)) {
+        let hundreds = Math.floor(sum/100);
+        let fifties = Math.floor((sum-(hundreds*100))/50);
+        let twenties = Math.floor((sum-(hundreds*100)-(fifties*50))/20)
+        let tens = Math.floor((sum-(hundreds*100)-(fifties*50)-(twenties*20))/10);
+        return console.log(`You will recieve ${hundreds} hundred € bill(s), ${fifties} fifty € bill(s), ${twenties} twenty € bill(s) and ${tens} ten € bill(s).`)
+    } 
+    else { console.log("You cannot retrieve this amount")}
+}
+
+atm()
