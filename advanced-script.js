671eeb332546f4189b0b7340a7884424c17a6dbd
Jakob Platzer <jakob.platzer@gmx.at>
2023-02-02 14:08:22 +0100

    advanced exercise 2 finished

--- advanced-script.js
+++ advanced-script.js
@@ -6,3 +6,14 @@ const capitalizedSentence = (function() {
     let firstLetter = sentence.charAt(0);
     return console.log(firstLetter.toUpperCase() + sentence.slice(1));
 }())
+
+
+// advanced exercise 2
+
+function calculateGrades(Math, Physics, English) {
+    let sum = Math+Physics+English;
+    let average = (Math+Physics+English)/3;
+    return console.log(`The sum of your grades is : ${sum}. \nThe average of your grades is: ${average}.`)
+}
+
+calculateGrades(3,4,5);
